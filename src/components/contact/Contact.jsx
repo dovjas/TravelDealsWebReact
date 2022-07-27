ebc7cfba86ca0ffc08cfed2114fdc12684369b08
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-07-27 22:16:52 +0300

    Contact form styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,6 +6,9 @@ const Contact = () => {
 <div className={styles.container}>
     <div className={styles.title}>
         <h1>Contact Us</h1>
+        <div className={styles.banner}>
+            <img src='https://img.freepik.com/free-vector/email-marketing-internet-chatting-24-hours-support_335657-3009.jpg?w=740&t=st=1658947679~exp=1658948279~hmac=428acd12c5b120c6fac89ae8d13b98c94d91271e97ef3f866349d46ea8c5673f'></img>
+    </div>
     </div>
     <form>
         <div className={styles.formInputs}>
@@ -19,7 +22,7 @@ const Contact = () => {
         <div className={styles.formInputs}>
             <textarea rows="4" cols="50"></textarea>
         </div>
-        <input type="submit" value="Submit"></input>
+        <button type="submit">Submit</button>
     </form>
 </div>
 
