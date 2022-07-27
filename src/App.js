71eba3f3e10f91346aeb9134abf0f7b41d7dab6b
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-07-27 10:57:51 +0300

    Offers component jsx and styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero"
 import Destinations from "./components/destinations/Destinations";
 import Travel from "./components/travel/Travel";
+import Offers from "./components/offers/Offers";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Destinations />
       <Travel />
+      <Offers />
     </div>
 )};
 
---
+++ src/components/offers/Offers.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styles from './Offers.module.css'
+
+
+const Offers = () => {
+  return (
+    <div className={styles.offers}>
+        <div className={styles.heading}>
+            <h2>Hot Offers</h2>
+            <div className={styles.textBg}>
+                <p>
+                    <span>Select from the most popular destinations</span>
+                </p>
+            </div>
+        </div>
+        <div className={styles.offersContainer}>
+            <div className={styles.offersCard}>
+                <img src="https://img.freepik.com/premium-vector/sale-special-offer-price-tags-design_1588-573.jpg?w=740"></img>
+                <div className={styles.content}>
+                    <h3>Offer 1</h3>
+                </div>
+            </div>
+            <div className={styles.offersCard}>
+                <img src="https://img.freepik.com/premium-vector/sale-special-offer-price-tags-design_1588-573.jpg?w=740"></img>
+                <div className={styles.content}>
+                    <h3>Offer 1</h3>
+                </div>
+            </div>
+            <div className={styles.offersCard}>
+                <img src="https://img.freepik.com/premium-vector/sale-special-offer-price-tags-design_1588-573.jpg?w=740"></img>
+                <div className={styles.content}>
+                    <h3>Offer 1</h3>
+                </div>
+            </div>
+            <div className={styles.offersCard}>
+                <img src="https://img.freepik.com/premium-vector/sale-special-offer-price-tags-design_1588-573.jpg?w=740"></img>
+                <div className={styles.content}>
+                    <h3>Offer 1</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Offers
