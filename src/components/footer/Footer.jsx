7627f97f050fcf2813074505e9fd312e30b78b13
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-07-31 13:17:01 +0300

    Footer styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,32 +5,32 @@ const Footer = () => {
   return (
     <div  className={styles.footer}>
         <div className={styles.container}>
-            <h2>Company</h2>
-            <ul>
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Destinations</a></li>
-                <li><a href='#'>Offers</a></li>
-                <li><a href='#'>Careers</a></li>
-
-            </ul>
-            <div className={styles.container}>
-            <h2>Social</h2>
-            <ul>
-                <li><a href='#'>Link</a></li>
-                <li><a href='#'>Link</a></li>
-                <li><a href='#'>Link</a></li>
-            </ul>
-            <div className={styles.container}>
-            <h2>Subsribe to our newsletter</h2>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-            <div className={styles.subsribe}>
-                <input type='email' placeholder='Enter your email'></input>
-                <button>Subscribe</button>
+            <div className={styles.companyContainer}>
+                <h2>Company</h2>
+                    <ul>
+                        <li><a href='#'>About</a></li>
+                        <li><a href='#'>Destinations</a></li>
+                        <li><a href='#'>Offers</a></li>
+                        <li><a href='#'>Careers</a></li>
+                    </ul>
+                    </div>
+                <div className={styles.socialContainer}>
+                    <h2>Social</h2>
+                    <ul>
+                        <li><a href='#'>Link</a></li>
+                        <li><a href='#'>Link</a></li>
+                        <li><a href='#'>Link</a></li>
+                    </ul>
+                </div>        
+                <div className={styles.subsContainer}>
+                    <h2>Subsribe to our newsletter</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <div className={styles.subsribe}>
+                        <input type='email' placeholder='Enter your email'></input>
+                    </div>
+                        <button>Subscribe</button>
+                </div>
             </div>
-            </div>
-           
-        </div>
-        </div>
     </div>
   )
 }
